b3a2c21e845ab457d3653b937c88403c73f24ff9
Francisco Revilla <paconte@gmail.com>
2022-01-27 19:14:01 +0100

    Fetch data from remote server. Transform fetched data into a matrix to represent a hmtl table.

--- src/service.js
+++ src/service.js
@@ -1,10 +1,5 @@
-const date = {
-	TODAY: "TODAY",
-	WEEK: "WEEK",
-	MONTH: "MONTH",
-}
-
 const data = [
+	//TODO: move to testing
 	[{id:1, time:"09:00 - 10:00"}, {id:2, status:"FREE"}, {id:3, status:"FREE"}],
   	[{id:4, time:"10:00 - 11:00"}, {id:5, status:"FREE"}, {id:6, status:"FREE"}],
   	[{id:7, time:"11:00 - 13:00"}, {id:8, status:"FREE"}, {id:9, status:"FREE"}],
@@ -12,10 +7,44 @@ const data = [
   	[{id:13, time:"13:00 - 14:00"}, {id:14, status:"FREE"}, {id:15, status:"FREE"}],
 ]
 
-export function fetchBookings(date) {
-	return data;
+async function fetchReservations() {
+	const url = 'http://195.201.148.68/reservations/getBookings4';
+	const response = await fetch(url);
+	const reservations = await response.json();
+	return reservations;
 }
 
-function wrapBookingTable(data) {
 
+function formatMinutes(minutes) {
+	let result = minutes;
+	if (minutes < 10) {
+		result = "0" + minutes;
+	}
+	return result;
+}
+
+
+function formatTime(timestamp) {
+	let date = new Date(timestamp);
+	let minutes = formatMinutes(date.getMinutes());
+	return date.getHours() + ":" + minutes;
+}
+
+
+export async function getReservationsTable() {
+	let result = Array();
+	const reservations = await fetchReservations();
+
+	reservations.forEach(element => {
+		let time =
+			formatTime(element.time.init * 1000)
+			+ " - "
+			+ formatTime(element.time.end * 1000);
+		let item = {id: -1, timeString: time};
+		let row = [item];
+		row = row.concat(element.items);
+		result.push(row);
+	});
+
+	return result;
 }
