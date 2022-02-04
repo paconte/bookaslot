92ad5f357714697d31723231166f02070fb7ff8c
Francisco Revilla <paconte@gmail.com>
2022-02-04 16:17:32 +0100

    Add new component DateNavigaion

--- src/BookingTable.svelte
+++ src/BookingTable.svelte
@@ -1,13 +1,23 @@
 <script>
-	import { bookings } from './stores.js';
-	import { getReservationsTable } from './service.js';
+	import { bookings, selectedDate } from './stores.js';
+	import {findIndex} from './service.js';
+
+	export let data;
+
 	let columns = ["Time", "Pista 1", "Pista 2"]
-	let data = getReservationsTable();
+	let currentData;
+	let index;
 
 	const status = {
 		FREE: "FREE",
 		BOOKED: "BOOKED",
 	}
+
+	/* STORE subscriptions */
+	selectedDate.subscribe(value => {
+		index = findIndex(data, value);
+		currentData = data[index].data;
+	});
 
 	/* STORE methods */
 	const addBooking = (item) => {
@@ -20,24 +30,31 @@
 		$bookings = $bookings.filter(element => element != item);
 	}
 
-	/* TABLE methods */
+	/* COMPONENT methods */
 	function getBackgroundColor(state) {
+		let result;
+
 		switch (state) {
-				case status.FREE:
-					return "bg-green-100";
-				case status.BOOKED:
-					return "bg-red-100";
-				case undefined:
-					return "";
-				default:
-					const text = 'Invalid argument state: ' + state;
-					console.error(text)
-					throw new TypeError(text);
+			case status.FREE:
+				result = "bg-green-100";
+				break;
+			case status.BOOKED:
+				result = "bg-red-100";
+				break;
+			case undefined:
+				result = "";
+				break;
+			default:
+				const text = 'Invalid argument state: ' + state;
+				console.error(text)
+				throw new TypeError(text);
 		}
+
+		return result;
 	}
 
-	function updateItemState(item, state, matrix) {
-		matrix.forEach(row => {
+	function updateItemState(item, state) {
+		currentData.forEach(row => {
 		  // find item
 			var index = row.map((cell) => cell.id).indexOf(item.id);
 			// update data
@@ -47,28 +64,26 @@
 				return;
 			};
 		});
-		return data;
+		currentData = currentData;
 	}
 
-	/* COMPONENT methods */
-	function bookItem(item, matrix) {
+	function bookItem(item) {
 		switch (item.status) {
 			case status.BOOKED:
-				data = updateItemState(item, status.FREE, matrix);
+				updateItemState(item, status.FREE);
 				deleteBooking(item);
 				break;
 			case status.FREE:
-				data = updateItemState(item, status.BOOKED, matrix);
+				updateItemState(item, status.BOOKED);
 				addBooking(item);
 				break;
 		}
 	}
+
 </script>
 
-
-<section class="container mx-auto p-6 font-mono">
-	<div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
-	  <div class="w-full overflow-x-auto">
+<div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+	<div class="w-full overflow-x-auto">
 		<table class="w-full">
 			<thead>
 				<tr class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
@@ -77,23 +92,21 @@
 					{/each}
 				</tr>
 			</thead>
+
 			<tbody class="bg-white">
-				{#await data then matrix}
-					{#each matrix as row}
+					{#each currentData as row}
 					<tr class="text-gray-700">
 						{#each row as cell}
-							<td class="px-4 py-3 border {getBackgroundColor(cell.status)}" on:click={() => bookItem(cell, matrix)}>
+							<td on:click={() => bookItem(cell)} class="px-4 py-3 border {getBackgroundColor(cell.status)}">
 								{(cell.timeString) ? cell.timeString : cell.status}
 							</td>
 						{/each}
 					</tr>
 					{/each}
-				{/await}
 			</tbody>
 		</table>
-	  </div>
 	</div>
-</section>
+</div>
 
 
 <p>
--- src/stores.js
+++ src/stores.js
@@ -2,3 +2,4 @@ import { writable } from 'svelte/store';
 
 //export const bookings = writable([{id:99, status:"BOOKED", bg:"bg-danger"}]);
 export const bookings = writable(new Set([]));
+export const selectedDate = writable();
