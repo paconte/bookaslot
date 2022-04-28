78d43bb01b9889d82403a0cdef5c63a041a504fc
Francisco Revilla <paconte@gmail.com>
2022-04-28 12:21:38 +0200

    Add dotenv dependency

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,10 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 import sveltePreprocess from 'svelte-preprocess'
+import replace from '@rollup/plugin-replace';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -38,6 +42,10 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
+		replace({
+			preventAssignment: true,
+            USER_NAME: JSON.stringify(process.env.USER_NAME)
+        }),
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
